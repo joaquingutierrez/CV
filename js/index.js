8dd45901b7481c95e38b4553f02c4794a4c44ff3
joaquingutierrez <joaquinguty@gmail.com>
2023-03-08 19:42:37 -0300

    Ahora los proyectos son cargados a traves de un json

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
+const cardContainer = document.getElementsByClassName("cardContainer")[0]
+
 const pedirInfo = () => {
-    console.log("hola")
     fetch("../data.json")
-    .then((data => data.json()))
-    .then((data) => console.log(data))
+        .then((data => data.json()))
+        .then((data) => {
+            let acum = ""
+            let acumTech = ""
+            data.map(card => {
+                card.technology.map(tec => {
+                    acumTech += `<p>${tec}</p>`
+                })
+                acum += `
+                    <div class="card">
+                        <a href=${card.linkToWebSite} target="_blank"><img src=${card.image} alt="Página web de fotografia"></a>
+                        <h3>${card.name}</h3>
+                        <div class="cardTechnology">
+                            ${acumTech}
+                        </div>
+                        <a href=${card.linkToRepo} target="_blank">Link to repo</a>
+                    </div>
+                `
+                acumTech = ""
+            })
+            cardContainer.innerHTML += acum
+        })
 }
 
 pedirInfo()
